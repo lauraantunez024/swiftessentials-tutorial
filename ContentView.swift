14ab1071bbfe3e0e3e72ce92b3ff657fbd2912dd
Laura Antunez <lauraantunez024@gmail.com>
2024-04-28 20:58:42 -0400

    first part of tutorial done

--- ContentView.swift
+++ ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-
+                MapView()
+                    .frame(height: 300)
+                CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
             VStack(alignment: .leading) {
                 Text("Turtle Rock")
                     .font(.title)
@@ -21,12 +25,19 @@ struct ContentView: View {
                     Text("California")
                         .font(.subheadline)
                 }
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
             .padding()
+            
+            Spacer()
                 
 
         }
-        .padding()
     }
 }
 
