84fc40e6497c65a2ff9b9b028fe6134383e89b65
Laura Antunez <lauraantunez024@gmail.com>
2024-04-29 22:54:07 -0400

    finished second part. UI part is done.

---
+++ Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Swiftessentials-tutorial
+//
+//  Created by Laura Antunez on 4/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
